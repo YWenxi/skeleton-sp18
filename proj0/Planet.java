d6ed025399e430f9dbaba54328026314cfa9880f
YWenxi <90666407+YWenxi@users.noreply.github.com>
2023-08-25 11:16:40 +0800

    finished proj0

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -7,7 +7,7 @@ public class Planet {
 	public String imgFileName;
 
 	// Constant
-	static final double G = 6.67e-11;
+	private static final double G = 6.67e-11;
 
 	// Constructor
 	public Planet(double xP, double yP, double xV, double yV, double m, String img) {
